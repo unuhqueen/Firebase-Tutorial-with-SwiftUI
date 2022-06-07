80eaf8e8b1397a458af4c977138394a9e6057a5c
Seungwon <csw98213@gmail.com>
2022-06-07 21:59:29 +0900

    Initial Commit

---
+++ firestore-demo/firestore_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  firestore_demoApp.swift
+//  firestore-demo
+//
+//  Created by Seungwon Choi on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct firestore_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
