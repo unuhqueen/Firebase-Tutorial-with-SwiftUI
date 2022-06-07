48ca91c556cddf6b3803182910fe1b4f749f4123
Seungwon <csw98213@gmail.com>
2022-06-08 00:04:41 +0900

    Feat: To do list connected with firestore

---
+++ firestore-demo/Models/Todo.swift
@@ -0,0 +1,15 @@
+//
+//  Todo.swift
+//  firestore-demo
+//
+//  Created by Seungwon Choi on 2022/06/07.
+//
+
+import Foundation
+
+struct Todo: Identifiable {
+    
+    var id: String
+    var name: String
+    var notes: String
+}
